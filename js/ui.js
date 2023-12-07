554816b6828dd6688e5c52b14fa79597358a31ef
Hazel Windle <hwindle83@googlemail.com>
2023-12-07 16:10:05 +0000

    Got the accordion FAQs working, adjusted animation

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,3 @@
-
 /*** mobile menu from left ***/
 const mobileBtn = document.querySelector('.header__mobile-menu-btn');
 
@@ -13,3 +12,19 @@ mobileBtn.addEventListener('click', showMenu, false);
 mobileBtn.addEventListener('touchstart', showMenu, false);
 
 /*** accordion in FAQs ***/
+
+const accordion = document.getElementsByClassName('q-and-a');
+//const questions = document.getElementsByClassName('question');
+//onst answers = document.getElementsByClassName('answer');
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.querySelector('.answer').classList.toggle('answer-hidden');
+    this.querySelector('.question').classList.toggle('closed');
+    this.querySelector('.question').classList.toggle('open');
+  });
+  accordion[i].addEventListener('touchstart', function () {
+    answers[i].classList.toggle('answer-hidden');
+    questions[i].classList.toggle('closed');
+    questions[i].classList.toggle('open');
+  });
+}
